b5e24ed60d2d8c4b4f452c623bfe2e6a0c7a72f3
Abiral Timalsina <timalsinaabiral4@gmail.com>
2025-10-26 11:45:55 +0545

    feat: add ApiError class for custom error handling

---
+++ utils/http.ts
@@ -0,0 +1,9 @@
+import { Request, Response, NextFunction } from 'express';
+
+export class ApiError extends Error {
+  status: number;
+  constructor(status: number, message: string) {
+    super(message);
+    this.status = status;
+  }
+}
