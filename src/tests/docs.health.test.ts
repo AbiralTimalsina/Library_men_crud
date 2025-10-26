0f26effd783beb9aa1911dcba4437259c62f83b4
PriyanshuShresth <csit23081040_priyanshu@achsnepal.edu.np>
2025-10-26 13:26:59 +0545

    Update docs.health.test.ts

--- src/tests/docs.health.test.ts
+++ src/tests/docs.health.test.ts
@@ -1,8 +1,8 @@
 import request from 'supertest';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
-import app from '../src/app.js';
-import { connect, disconnect } from '../src/db/mongoose.js';
+import app from '../app.js';
+import { connect, disconnect } from '../db/mongoose.js';
 
 let mongod: MongoMemoryServer;
 
