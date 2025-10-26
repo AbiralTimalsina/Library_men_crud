b0c2b6b74346f532b16563edffc6fd06e171a7b0
Jungeey <jungkhatrisudip@gmail.com>
2025-10-26 12:32:01 +0545

    feat:added routes of book and member

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,10 @@
 import { Router } from 'express';
+import bookRoutes from './book.routes.js';
+import memberRoutes from './member.routes.js';
 
 const api = Router();
+
+api.use('/books', bookRoutes);
+api.use('/members', memberRoutes);
 
 export default api;
