ed3259b99efcea5d2b242ccfa5df0e0c2d8c1974
Abiral Timalsina <timalsinaabiral4@gmail.com>
2025-10-26 12:20:44 +0545

    index.ts updated

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,5 @@
 import { Router } from 'express';
-import bookRoutes from './book.routes.js';
-import memberRoutes from './member.routes.js';
+
 const api = Router();
-api.use('/books', bookRoutes);
-api.use('/members', memberRoutes);
+
 export default api;
